b0a6b63ecf08a69744a4b34cdfb38571247dcdbb
alvarocm97 <78368277+alvarocm97@users.noreply.github.com>
2021-04-15 01:33:12 +0200

    Actualizacion

--- SeleccionarEntidades/Widget.js
+++ SeleccionarEntidades/Widget.js
@@ -1,16 +1,15 @@
-define(['dojo/_base/declare', 'jimu/BaseWidget', 'esri/symbols/SimpleFillSymbol', "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", "esri/layers/FeatureLayer", 'dojo/_base/lang', 'dojo/dom', 'esri/graphic', 'esri/Color', "esri/toolbars/draw", "dojo/store/Memory", "esri/tasks/query", "dgrid/OnDemandGrid", "dgrid/Selection", "dojo/_base/array",], function (declare, BaseWidget, SimpleFillSymbol, SimpleMarkerSymbol, SimpleLineSymbol, FeatureLayer, lang, dom, Graphic, Color, Draw,  Memory, Query, Grid, Selection, array) {
+define(['dojo/_base/declare', 'jimu/BaseWidget', "esri/symbols/SimpleMarkerSymbol", "esri/layers/FeatureLayer", 'dojo/_base/lang', 'dojo/dom', "esri/toolbars/draw", "dojo/store/Memory", "esri/tasks/query", "dgrid/OnDemandGrid", "dgrid/Selection", "dojo/_base/array",], function (declare, BaseWidget, SimpleMarkerSymbol, FeatureLayer, lang, dom, Draw,  Memory, Query, Grid, Selection, array) {
   return declare([BaseWidget], {
-    baseClass: 'add-graphic',    
-    
-    onOpen: function() {      
+    baseClass: 'add-graphic',     
 
-      // var symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([255, 255, 0]), 2), new Color([255, 255, 0, 0.2]));    
+    onOpen: function() {             
 
       console.log("Parte1");
 
-      var featurelayer = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/0";
+      var outFieldsUrban = ["objeto", "estado"];
+      
 
-      var outFieldsUrban = ["ID", "Estado"];
+      var featurelayer = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/0";      
 
             
             var lyrUrban = new FeatureLayer(featurelayer, {
@@ -18,31 +17,58 @@ define(['dojo/_base/declare', 'jimu/BaseWidget', 'esri/symbols/SimpleFillSymbol'
                 outFields: outFieldsUrban
 
             });            
-            this.map.addLayers([lyrUrban]);              
+      var featurelayer1 = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/1";
+            
+            var lyrUrban1 = new FeatureLayer(featurelayer1, {
+                
+                outFields: outFieldsUrban
 
-      // this.map.graphics.show();
-      
-      // this.map.on('click', lang.hitch(this, function (evt) {
-      //   if (dom.byId('activado').checked) {
+            });            
+      var featurelayer2 = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/2";
+            
+            var lyrUrban2 = new FeatureLayer(featurelayer2, {
+                
+                outFields: outFieldsUrban
 
-      //     console.log("EVT", evt)
+            });            
+      var featurelayer3 = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/3";
+            
+            var lyrUrban3 = new FeatureLayer(featurelayer3, {
+                
+                outFields: outFieldsUrban
 
-      //     this.map.graphics.clear();
+            });            
+      var featurelayer4 = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/4";
+            
+            var lyrUrban4 = new FeatureLayer(featurelayer4, {
+                
+                outFields: outFieldsUrban
 
-      //     var geometryInput = evt.mapPoint;
+            });            
+      var featurelayer5 = "https://desktop-smcdlo2/server/rest/services/PFM/Servicios_Empleados/FeatureServer/5";
+            
+            var lyrUrban5 = new FeatureLayer(featurelayer5, {
+                
+                outFields: outFieldsUrban
 
-      //     var graphic = new Graphic(geometryInput, symbol);
+            });            
+            this.map.addLayers([lyrUrban]);             
+            this.map.addLayers([lyrUrban1]);             
+            this.map.addLayers([lyrUrban2]);             
+            this.map.addLayers([lyrUrban3]);             
+            this.map.addLayers([lyrUrban4]);             
+            this.map.addLayers([lyrUrban5]);
+                         
 
-      //     this.map.graphics.add(graphic);
+      if (dom.byId("activado").checked){
+        
 
-      //     selectPoint(geometryInput)
-      //   };
-      // }));
       var tbDraw = new Draw(this.map);
 
                 tbDraw.on("draw-complete", selectPoint);
 
                 tbDraw.activate(Draw.POLYGON);
+      
 
       function selectPoint(geometryInput) {
 
@@ -60,6 +86,11 @@ define(['dojo/_base/declare', 'jimu/BaseWidget', 'esri/symbols/SimpleFillSymbol'
           });
           
           lyrUrban.setSelectionSymbol(symbolSelected);
+          lyrUrban1.setSelectionSymbol(symbolSelected);
+          lyrUrban2.setSelectionSymbol(symbolSelected);
+          lyrUrban3.setSelectionSymbol(symbolSelected);
+          lyrUrban4.setSelectionSymbol(symbolSelected);
+          lyrUrban5.setSelectionSymbol(symbolSelected);
           
           
           var queryUrban = new Query();
@@ -68,26 +99,40 @@ define(['dojo/_base/declare', 'jimu/BaseWidget', 'esri/symbols/SimpleFillSymbol'
 
                 
                 lyrUrban.on("selection-complete", populateGrid);
+                lyrUrban1.on("selection-complete", populateGrid1);
+                lyrUrban2.on("selection-complete", populateGrid2);
+                lyrUrban3.on("selection-complete", populateGrid3);
+                lyrUrban4.on("selection-complete", populateGrid4);
+                lyrUrban5.on("selection-complete", populateGrid5);
 
                 
                 lyrUrban.selectFeatures(queryUrban, FeatureLayer.SELECTION_NEW);
+                lyrUrban1.selectFeatures(queryUrban, FeatureLayer.SELECTION_NEW);
+                lyrUrban2.selectFeatures(queryUrban, FeatureLayer.SELECTION_NEW);
+                lyrUrban3.selectFeatures(queryUrban, FeatureLayer.SELECTION_NEW);
+                lyrUrban4.selectFeatures(queryUrban, FeatureLayer.SELECTION_NEW);
+                lyrUrban5.selectFeatures(queryUrban, FeatureLayer.SELECTION_NEW);
 
-          };
+          };   
+               
 
       function populateGrid(results) {
 
-        console.log(results)
+        // arrayNuevo.push(results.features)
+
+        console.log("Results", results)
 
         var gridUrbanismo = new (declare([Grid, Selection]))({
           bufferRows: Infinity,
           columns: {
-              objeto: "ID",              
+              objeto: "Sanecan",              
               estado: "Estado"              
           }
       }, "divGrid");
          
           
           var dataUrbanismo = array.map(results.features, function (feature) {
+            console.log("Feature", feature)
               var outFieldsUrban = ["objeto", "estado"];
               return {              
                   /*
@@ -107,8 +152,181 @@ define(['dojo/_base/declare', 'jimu/BaseWidget', 'esri/symbols/SimpleFillSymbol'
 
           console.log("Hola")
       };
-    }
+      function populateGrid1(results) {
 
-    
+        // arrayNuevo.push(results.features)
+
+        console.log("Results", results)
+
+        var gridUrbanismo = new (declare([Grid, Selection]))({
+          bufferRows: Infinity,
+          columns: {
+              objeto: "Papeleras",              
+              estado: "Estado"              
+          }
+      }, "divGrid1");         
+          
+          var dataUrbanismo = array.map(results.features, function (feature) {
+            console.log("Feature", feature)
+              var outFieldsUrban = ["objeto", "estado"];
+              return {              
+                  /*
+                   * Step: Reference the attribute field values
+                   */
+                  "objeto": feature.attributes[outFieldsUrban[0]],
+                  "estado": feature.attributes[outFieldsUrban[1]]
+              }
+          });
+          console.log(dataUrbanismo)
+
+          // Pass the data to the grid
+          var memStore = new Memory({
+              data: dataUrbanismo
+          });
+          gridUrbanismo.set("store", memStore);
+
+          console.log("Hola")
+      };
+      function populateGrid2(results) {
+
+        // arrayNuevo.push(results.features)
+
+        console.log("Results", results)
+
+        var gridUrbanismo = new (declare([Grid, Selection]))({
+          bufferRows: Infinity,
+          columns: {
+              objeto: "Fuentes",              
+              estado: "Estado"              
+          }
+      }, "divGrid2");
+         
+          
+          var dataUrbanismo = array.map(results.features, function (feature) {
+            console.log("Feature", feature)
+              var outFieldsUrban = ["objeto", "estado"];
+              return {              
+                  /*
+                   * Step: Reference the attribute field values
+                   */
+                  "objeto": feature.attributes[outFieldsUrban[0]],
+                  "estado": feature.attributes[outFieldsUrban[1]]
+              }
+          });
+          console.log(dataUrbanismo)
+
+          // Pass the data to the grid
+          var memStore = new Memory({
+              data: dataUrbanismo
+          });
+          gridUrbanismo.set("store", memStore);
+
+          console.log("Hola")
+      };
+      function populateGrid3(results) {
+
+        // arrayNuevo.push(results.features)
+
+        console.log("Results", results)
+
+        var gridUrbanismo = new (declare([Grid, Selection]))({
+          bufferRows: Infinity,
+          columns: {
+              objeto: "Bancos",              
+              estado: "Estado"              
+          }
+      }, "divGrid3");         
+          
+          var dataUrbanismo = array.map(results.features, function (feature) {
+            console.log("Feature", feature)
+              var outFieldsUrban = ["objeto", "estado"];
+              return {              
+                  /*
+                   * Step: Reference the attribute field values
+                   */
+                  "objeto": feature.attributes[outFieldsUrban[0]],
+                  "estado": feature.attributes[outFieldsUrban[1]]
+              }
+          });
+          console.log(dataUrbanismo)
+
+          // Pass the data to the grid
+          var memStore = new Memory({
+              data: dataUrbanismo
+          });
+          gridUrbanismo.set("store", memStore);
+
+          console.log("Hola")
+      };
+      function populateGrid4(results) {
+
+        // arrayNuevo.push(results.features)
+
+        console.log("Results", results)
+
+        var gridUrbanismo = new (declare([Grid, Selection]))({
+          bufferRows: Infinity,
+          columns: {
+              objeto: "Arbolado",              
+              estado: "Estado"              
+          }
+      }, "divGrid4");         
+          
+          var dataUrbanismo = array.map(results.features, function (feature) {
+            console.log("Feature", feature)
+              var outFieldsUrban = ["objeto", "estado"];
+              return {              
+                  /*
+                   * Step: Reference the attribute field values
+                   */
+                  "objeto": feature.attributes[outFieldsUrban[0]],
+                  "estado": feature.attributes[outFieldsUrban[1]]
+              }
+          });
+          console.log(dataUrbanismo)
+
+          // Pass the data to the grid
+          var memStore = new Memory({
+              data: dataUrbanismo
+          });
+          gridUrbanismo.set("store", memStore);
+
+          console.log("Hola")
+      };
+      function populateGrid5(results) {
+
+        // arrayNuevo.push(results.features)
+
+        console.log("Results", results)
+
+        var gridUrbanismo = new (declare([Grid, Selection]))({
+          bufferRows: Infinity,
+          columns: {
+              objeto: "Parques Infantiles",              
+              estado: "Estado"              
+          }
+      }, "divGrid5");         
+          
+          var dataUrbanismo = array.map(results.features, function (feature) {
+            console.log("Feature", feature)
+              var outFieldsUrban = ["objeto", "estado"];
+              return {              
+                  /*
+                   * Step: Reference the attribute field values
+                   */
+                  "objeto": feature.attributes[outFieldsUrban[0]],
+                  "estado": feature.attributes[outFieldsUrban[1]]
+              }
+          });
+          console.log(dataUrbanismo)
+
+          // Pass the data to the grid
+          var memStore = new Memory({
+              data: dataUrbanismo
+          });
+          gridUrbanismo.set("store", memStore);          
+      };
+    };
+}    
   });
 });
